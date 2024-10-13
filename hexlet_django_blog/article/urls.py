ae1761845130fc960ab1b1aefaee481581bb005b
UsmanSakhbeev <sahbeyev@gmail.com>
2024-10-13 15:34:08 +0300

    added routing

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from hexlet_django_blog.article import views
 
-from .views import ArticleIndexView
-
 urlpatterns = [
-    path("", ArticleIndexView.as_view(), name="article_index"),
+    path("", views.home, name="home"),
+    path("articles/<str:tags>/<int:article_id>", views.index, name="article"),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,16 +1,12 @@
-from django.http import HttpResponse
-from django.views import View
+from django.shortcuts import HttpResponse, redirect
+from django.urls import reverse
 
 
-class ArticleIndexView(View):
-    def get(self, request, *args, **kwargs):
-        context = {"app_name": "Articles"}
-        return HttpResponse(context)
+def index(request, tags, article_id):
+    return HttpResponse(f"Статья номер {tags}. Тег {article_id}")
 
 
-"""
-def index(request):
-    # Передаем название приложения через контекст
-    context = {"app_name": "Articles"}
-    return render(request, "articles/index.html", context)
-"""
+def home(request):
+    url = reverse("article", kwargs={"tags": "python", "article_id": 42})
+    return redirect(url)
+    
